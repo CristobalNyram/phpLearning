95399d2d6f0388719252fd40b6c1ccbced9c669a
cristobal nyram <crisco.nyram@gmail.com>
2022-03-09 09:52:18 -0600

    formulario de escuela

--- index.php
+++ index.php
@@ -75,6 +75,29 @@
           </form>
 
     </div>
+
+
+
+    <div class="container m-5 mt-5">
+        <h1 class="text-center m-3 mt-5">
+            Registro de escuelas
+        </h1>
+
+        <form>
+            <div class="mb-3">
+              <label for="nombre" class="form-label">Nombre</label>
+              <input type="email" class="form-control" id="nombre" aria-describedby="nombre">
+
+              <label for="direccion" class="form-label">Direccion</label>
+              <input type="email" class="form-control" id="direccion" aria-describedby="nombre">
+
+            </div>
+
+       
+            <button type="submit" class="btn btn-primary">Registrar</button>
+          </form>
+
+    </div>
     
 </body>
 </html>
